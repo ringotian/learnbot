9faa9ef5726f0e1e51816291cc16c3a05b91e4b3
Anna Yablunovskaya <lad.shada@gmail.com>
2019-09-19 00:21:50 +0300

    added planet comand

--- bot.py
+++ bot.py
@@ -1,36 +1,56 @@
+"""
+Домашнее задание №1
+Использование библиотек: ephem
+* Установите модуль ephem
+* Добавьте в бота команду /planet, которая будет принимать на вход 
+  название планеты на английском, например /planet Mars
+* В функции-обработчике команды из update.message.text получите 
+  название планеты (подсказка: используйте .split())
+* При помощи условного оператора if и ephem.constellation научите 
+  бота отвечать, в каком созвездии сегодня находится планета.
+"""
+import logging, settings, ephem, datetime
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-import logging
-import settings
-
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
-                    )
+)
+
 
 def greet_user(bot, update):
-    text = 'Start triggered'
-    logging.info('Start triggered')
+    text = 'Вызван /start'
+    print(text)
     update.message.reply_text(text)
 
-def talk_to_me(bot, update):
-    user_text = "Hi {}! You wrote: {}".format(update.message.chat.first_name, update.message.text) 
-    logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username,
-                update.message.chat.id, update.message.text)
-    update.message.reply_text(user_text)
-    
 
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+def check_constellation(bot, update):
+    all_known_planets = ['Mars','Mercury','Jupiter','Neptune','Venus','Saturn','Uranus','Moon']
+    planet_name = update.message.text.split()[1]
+    if planet_name in all_known_planets:
+        current_date = datetime.datetime.now()
+        result_constellation = ephem.constellation(getattr(ephem,planet_name)(current_date))[1]
+        update.message.reply_text(f'{planet_name} now in {result_constellation} constellation')
+    else:
+        update.message.reply_text("I know nothing about this planet")
+ 
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
-
-    logging.info('Bot is starting...')
-
+    
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    #dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("planet", check_constellation))
 
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     
     mybot.start_polling()
     mybot.idle()
+       
 
-main()
+if __name__ == "__main__":
+    main()
