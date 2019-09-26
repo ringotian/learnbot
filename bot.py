d88050df57652e9097d2d32a9da8e11faad7063a
Anna Yablunovskaya <lad.shada@gmail.com>
2019-09-26 19:31:16 +0300

    add cats command

--- bot.py
+++ bot.py
@@ -9,8 +9,16 @@
 * При помощи условного оператора if и ephem.constellation научите 
   бота отвечать, в каком созвездии сегодня находится планета.
 """
-import logging, settings, ephem, datetime
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging, datetime
+from glob import glob
+from random import choice
+
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler
+import ephem
+from  emoji import emojize
+
+import settings
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -18,16 +26,19 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 )
 
 
-def greet_user(bot, update):
+def greet_user(bot, update, user_data):
     text = 'Вызван /start'
-    print(text)
-    update.message.reply_text(text)
+    emo = emojize(choice(settings.USER_EMOJI), use_aliases = True)
+    my_keyboard = ReplyKeyboardMarkup([['Прислать котика']])
+    user_data['emo'] = emo    
+    text = 'Привет {}'.format(emo)
+    update.message.reply_text(text, reply_markup=my_keyboard)
 
-def talk_to_me(bot, update):
-    user_text = update.message.text 
-    print(user_text)
+def talk_to_me(bot, update, user_data):
+    user_text = "Привет {} {}! Ты написал: {}".format(update.message.chat.first_name, user_data['emo'],
+                update.message.text)
     update.message.reply_text(user_text)
-
+    
 def check_constellation(bot, update):
     all_known_planets = ['Mars','Mercury','Jupiter','Neptune','Venus','Saturn','Uranus','Moon']
     planet_name = update.message.text.split()[1]
@@ -56,17 +67,25 @@ def word_count(bot, update):
     else: 
         update.message.reply_text('Вы ничего не ввели')
 
+def send_cat_picture(bot, update, user_data):
+    cat_list = glob('images/cat*.jpg')
+    cat_pic = choice(cat_list)
+    bot.send_photo(chat_id=update.message.chat_id, photo=open(cat_pic,'rb'))
+
+
 
 def main():
     mybot = Updater(settings.API_KEY)
     
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    #dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("start", greet_user, pass_user_data=True))
     dp.add_handler(CommandHandler("planet", check_constellation))
     dp.add_handler(CommandHandler('next_full_moon', full_moon))
     dp.add_handler(CommandHandler("wordcount", word_count))
-    
+    dp.add_handler(CommandHandler("cat", send_cat_picture, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(Прислать кошечку)$', send_cat_picture, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
+
     mybot.start_polling()
     mybot.idle()
        
